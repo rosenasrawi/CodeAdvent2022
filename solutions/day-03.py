64ba25dab336dda0b06ec1b2b1f9ef2ef2936649
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-03 11:10:32 +0100

    Refactor day 3

--- solutions/day-03.py
+++ solutions/day-03.py
@@ -7,16 +7,17 @@ def split(sack):
     m = len(sack)//2
     return sack[:m], sack[m:]
 
-letters = list(string.ascii_letters)
-priorities = list(range(1,27))+list(range(27,53))
-mappings = {letters[i]: priorities[i] for i in range(len(letters))}
+let = list(string.ascii_letters)
+pri = list(range(1,27))+list(range(27,53))
+mappings = {let[i]: pri[i] for i in range(len(pri))}
 
-def elfsupplies(rucksacks, part, common = []):
+def elfsupplies(rucksacks, part, common = [], total = 0):
 
     if part == 1:
 
         for sack in rucksacks:
             s1, s2 = split(sack)
+
             for i in s1:
                 if i in s2: common.append(i); break
 
@@ -24,16 +25,13 @@ def elfsupplies(rucksacks, part, common = []):
 
         for s in range(0,len(rucksacks),3):
             group = rucksacks[s:s+3]
-            self = max(group, key=len)
-            group.pop(group.index(self))
+            self = group.pop(group.index(max(group, key=len)))
 
             for i in self:
                 if i in group[0] and i in group[1]: 
                     common.append(i); break
 
-    total = 0
-    for i in common:
-        total+=mappings[i]
+    for i in common: total+=mappings[i]
 
     return total
 
