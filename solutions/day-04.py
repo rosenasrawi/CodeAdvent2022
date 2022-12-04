ab863a3e4b94c0a9ebc1fb9c17ee764653986c55
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-04 09:38:46 +0100

    One function for both parts

--- solutions/day-04.py
+++ solutions/day-04.py
@@ -3,28 +3,22 @@
 from _preprocess import *
 import re
 
-def partoverlap(assignments, total = 0):
+def findoverlap(assignments, part=0, full=0):
 
     for asg in assignments:
 
         overlap = (asg[0] >= asg[2] and asg[1] <= asg[3]) or (asg[2] >= asg[0] and asg[3] <= asg[1])
-        if overlap: total += 1
-
-    print('Part 1:', total)
-
-def fulloverlap(assignments, total = 0):
-
-    for asg in assignments:
+        if overlap: part += 1   
 
         s1 = list(range(asg[0],asg[1]+1))
         s2 = list(range(asg[2],asg[3]+1))
 
         for i in s1:
-            if i in s2: total +=1; break
+            if i in s2: full +=1; break      
 
-    print('Part 2:', total)
-    
+    print('Part 1:', part, ',', 'Part 2:', full)
+
 assignments = preprocess('04')
 assignments = [list(map(int,re.split('-|,', asg))) for asg in assignments]
 
-partoverlap(assignments); fulloverlap(assignments)
+findoverlap(assignments)
