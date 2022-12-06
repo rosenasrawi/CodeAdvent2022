ddefbb687c43495a9481e79ccc014d943018eac5
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-06 15:40:37 +0100

    Auto get input data

--- solutions/_newday.py
+++ solutions/_newday.py
@@ -1,11 +1,15 @@
 from datetime import date
+from aocd import get_data
 
 today = date.today()
 day = today.strftime('%d')
 
-solfile = 'day-' + day + '.py'
-f = open('solutions/' + solfile, 'x')       # Returns error if file already exists
-f.write('from _preprocess import *')        
+input = get_data(day = int(day), year = 2022)
 
 inpfile = 'input-' + day + '.txt'
-i = open('input/' + inpfile, 'x')
+i = open('input/' + inpfile, 'x')           # Returns error if file already exists
+i.write(input)
+
+solfile = 'day-' + day + '.py'
+f = open('solutions/' + solfile, 'x')       # Returns error if file already exists
+f.write('from _preprocess import *')
