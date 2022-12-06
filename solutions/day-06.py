593057bd143e6515db642c744b840f5ba0dc48dd
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-06 12:08:42 +0100

    Refactor day 6

--- solutions/day-06.py
+++ solutions/day-06.py
@@ -1,16 +1,11 @@
 from _preprocess import *
 from collections import Counter
 
-signal = preprocess('06')[0]
-
 def findstart(signal,plen):
-
     for i in range(len(signal)-plen-1):
-        pack = signal[i:i+plen]
-        if len(pack) == len(Counter(pack)):
-            break
+        if len(Counter(signal[i:i+plen])) == plen:
+            return i+plen; break
 
-    return i+plen
-
+signal = preprocess('06')[0]
 print('Part 1:', findstart(signal,4))
 print('Part 2:', findstart(signal,14))
