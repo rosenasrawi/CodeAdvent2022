6459f9aa96b43cded5a46e13772dfe36fed9f24a
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-08 11:47:40 +0100

    Solve day 8

---
+++ solutions/day-08.py
@@ -0,0 +1,46 @@
+# Day 8: Treetop Tree House
+
+from _preprocess import *
+
+def viewdist(line, tree, v = 0):
+    for t in line:
+        v += 1
+        if int(t) >= tree: break
+    return v
+
+def treehouse(trees, visible = 0, view = []):
+
+    for r in range(1,len(trees)-1):
+
+        hor = trees[r]
+
+        for c in range(1,len(trees)-1):
+
+            vert = ''.join([trees[i][c] for i in range(len(trees))])
+            tree = int(trees[r][c])
+            
+            left = hor[:c][::-1]; right = hor[c+1:]; 
+            top = vert[:r][::-1]; bottom = vert[r+1:]
+
+            lvis = all(int(i) < tree for i in left)
+            rvis = all(int(i) < tree for i in right)
+            tvis = all(int(i) < tree for i in top)
+            bvis = all(int(i) < tree for i in bottom)
+
+            if lvis or rvis or tvis or bvis:
+                visible += 1
+
+            lview = viewdist(left,tree)
+            rview = viewdist(right,tree)
+            tview = viewdist(top,tree)
+            bview = viewdist(bottom,tree)
+            
+            view.append(lview*rview*tview*bview)
+
+    visedge = len(trees)*2 + (len(trees)-2)*2
+    visible += visedge
+
+    return visible, max(view)
+
+visible, view = treehouse(preprocess('08'))
+print('Part 1:', visible); print('Part 2:', view)
