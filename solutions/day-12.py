72667eaf48fdde88b9febaa86abd8e26d623038d
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-12 18:40:08 +0100

    Refactor day 12

--- solutions/day-12.py
+++ solutions/day-12.py
@@ -3,7 +3,7 @@
 from _preprocess import *
 import string
 
-def adjacent(next, rmax, cmax, adj = {'l':[], 'r':[], 'u':[], 'd':[]}):
+def neighbour(next, rmax, cmax, adj = {'l':[], 'r':[], 'u':[], 'd':[]}):
     r, c = next
 
     adj['l'] = [r,c-1]; adj['r'] = [r,c+1]
@@ -16,65 +16,45 @@ def adjacent(next, rmax, cmax, adj = {'l':[], 'r':[], 'u':[], 'd':[]}):
     
     return list(adj.values())
 
-def step(l1, l2, reverse = False, letters = list(string.ascii_lowercase)):
+def makestep(l1, l2, reverse = False):
+    
+    letters = list(string.ascii_lowercase)
+    if reverse: letters.reverse()
+
     if l1 == 'S': l1 = 'a'
-    if l2 == 'E': l2 = 'z'
     if l1 == 'E': l1 = 'z'
+    if l2 == 'E': l2 = 'z'
 
-    if reverse:
-        if letters.index(l1) - letters.index(l2) < 2:
-            return True
-        else: return False   
-    else:    
-        if letters.index(l2) - letters.index(l1) < 2:
-            return True
-        else: return False
+    if letters.index(l2) - letters.index(l1) < 2:
+        return True
+    else: return False
 
-def findstart(map, reverse = False):
+def findpath(map, s, e, reverse = False):
 
     for i, r in enumerate(map):
-        if reverse:
-            if 'E' in r: start = [i,r.index('E')]
-        else:
-            if 'S' in r: start = [i,r.index('S')]
+        if s in r: start = [i,r.index(s)]
 
-    return start
-
-def distmat(map, distances, dist = []):
-
+    visited = []; queue = [start]
+    distances = []; dist = []
     for i in range(len(map[0])): dist.append(0)
     for i in range(len(map)): distances.append(dist.copy())
 
-    return distances
-
-def findpath(map, reverse = False):
-
-    start = findstart(map, reverse)
-    visited = []; queue = [start]
-    distances = distmat(map, distances = [])
-
     while queue:  
+
         start = queue.pop(0)
         l1 = map[start[0]][start[1]]
 
-        if reverse:
-            if l1 == 'a': 
-                visited.append(start); break
-        else:
-            if l1 == 'E': 
-                visited.append(start); break
+        if l1 == e: visited.append(start); break
+        if start not in visited: visited.append(start)
 
-        if start not in visited:
-            visited.append(start)
-
-        neighbours = adjacent(start,len(map),len(map[0]))
+        adj = neighbour(start,len(map),len(map[0]))
         olddist = distances[start[0]][start[1]]
 
-        for next in neighbours:
+        for next in adj:
             l2 = map[next[0]][next[1]]
 
             if next not in visited:
-                if step(l1,l2,reverse):
+                if makestep(l1,l2,reverse):
                     distances[next[0]][next[1]] = olddist + 1
                     visited.append(next)
                     queue.append(next)
@@ -85,5 +65,5 @@ def findpath(map, reverse = False):
 
 map = preprocess('12')
 
-print('Part 1:', findpath(map))
-print('Part 2:', findpath(map,reverse = True))
+print('Part 1:', findpath(map, s = 'S', e = 'E'))
+print('Part 2:', findpath(map, s = 'E', e = 'a', reverse = True))
