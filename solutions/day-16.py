bbd3da9fdd3fd194fa3e3121dbddbd4bf6b103ec
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-16 16:54:50 +0100

    Solve day 16

--- solutions/day-16.py
+++ solutions/day-16.py
@@ -3,25 +3,17 @@
 from _getinput import *
 import re
 
-scanner = getinput('ex')
+def getvalves(scanner, valves = {}, flows = {}):
 
-valves = {}
-flows = {}
+    for out in scanner:
+        v = list(map(str,re.findall(r'\b[A-Z]{2}\b', out)))
+        f = int(re.findall(r'\d+', out)[0])
+        valves[v[0]] = v[1:]
+        flows[v[0]] = f
 
-for out in scanner:
-    v = list(map(str,re.findall(r'\b[A-Z]{2}\b', out)))
-    f = int(re.findall(r'\d+', out)[0])
-    valves[v[0]] = v[1:]
-    flows[v[0]] = f
+    return valves, flows
 
-print(valves)
-print(flows)
-
-def dfs(flows, valves):
-
-    stack = [(1,"AA", 0, ())]
-    memory = {}
-    best = 0
+def dfs(flows, valves, stack = [(1,"AA", 0, ())], memory = {}, best = 0):
 
     while stack:
 
@@ -38,100 +30,76 @@ def dfs(flows, valves):
             if score > best: best = score
             continue # Times up, so try next one
 
-        # (1) go to neighbour:
         flow = sum(flows[loc] for loc in opened)
-        for neigh in valves[location]:
+
+        for neigh in valves[location]: # (1) go to neighbour
             newstate = (time+1, neigh, score+flow, tuple(opened))
             stack.append(newstate)
 
-        # (2) open the valve:
-        if flows[location] > 0 and location not in opened:
+        if flows[location] > 0 and location not in opened: # (2) open the valve
             opened.add(location)
             flow = sum(flows[loc] for loc in opened)
 
             newstate = (time+1, location, score+flow, tuple(opened))
             stack.append(newstate)
-    print(memory)
-    return best
-            
-print(dfs(flows, valves))
-
-
-# values = {'t':1, 'start': 'AA', 'score': 0, 'path': ['AA']}
-
-# def dfs(flows, valves, values):
-
-#     queue = [(1, 'AA', 0, ('AA',))]
-#     visited = {}
-#     best = 0
-
-#     while queue:
-#         next = queue.pop()
-#         time, loc, score, opened = next
-
-#         if visited.get((time,loc), -1) >= score:
-#             continue
-
-#         visited[(time,loc)] = score
-
-def solve(flows, valves):
-
-    states = [(1, "AA", 0, ())]
-    seen = {}
-    best = 0
-
-    while len(states) > 0:
-
-        current = states.pop()
-        time, where, score, opened_s = current
-        opened = {x for x in opened_s}
-
-        if seen.get((time, where), -1) >= score:
-            continue
-
-        seen[(time, where)] = score
-
-        if time == 30:
-            best = max(best, score)
-            continue
-
-        # if we don't open a valve here
-
-        new_score = score + sum(flows.get(where, 0) for where in opened)
-        for option in valves[where]:
-            new_state = (time + 1, option, new_score, tuple(opened))
-            states.append(new_state)
-
-        # if we open the valve here
-        if flows[where] > 0 and where not in opened:
-
-            opened.add(where)
-            # new_score = score + sum(flows.get(where, 0) for where in opened)
-            new_score = score + sum(flows[loc] for loc in opened) 
-            new_state = (time + 1, where, new_score, tuple(opened))
-
-            states.append(new_state)
-            # opened.discard(where)
-
 
     return best
 
-# print(solve(flows, valves))
+def elephantdfs(flows, valves, stack = [(1, "AA", "AA", 0, ())], memory = {}, best = 0):
 
-# def find_all_paths(graph, start, end, path=[]):
+    while stack:
 
-#     path = path + [start]
-#     if start == end:
-#         return [path]
+        state = stack.pop()
+        time, me, elephant, score, opened = state
+        opened = {x for x in opened}
 
-#     paths = []
+        if memory.get((time,me,elephant), -1) >= score: # Had better score here before
+            continue # Skip to the next iteration
 
-#     for node in graph[start]:
-#         if node not in path:
-#             newpaths = find_all_paths(graph, node, end, path)
-#             for newpath in newpaths:
-#                 paths.append(newpath)
+        memory[(time,me,elephant)] = score # Save score for this time & location
 
-#     return paths
+        if time == 26:
+            if score > best: best = score
+            continue # Times up, so try next one
 
-# print(find_all_paths(valves, 'AA', 'BB'))
+        flow = sum(flows[loc] for loc in opened)
+        
+        mecanopen = (flows[me] > 0 and me not in opened)
+        elephantcanopen = (flows[elephant] > 0 and elephant not in opened)
+
+        for neigh_me in valves[me]: # (1) Both go to neighbour
+            for neigh_eleph in valves[elephant]:
+                newstate = (time+1, neigh_me, neigh_eleph, score+flow, tuple(opened))
+                stack.append(newstate)
+
+        if elephantcanopen: # (2) Only elephant opens valve
+            opened.add(elephant)
+            flow = sum(flows[loc] for loc in opened)
+
+            for neigh_me in valves[me]: 
+                newstate = (time+1, neigh_me, elephant, score+flow, tuple(opened))
+                stack.append(newstate)
+
+            opened.discard(elephant) # remove for when they don't
+
+        if mecanopen: # (3) Only I open valve
+            opened.add(me)
+            flow = sum(flows[loc] for loc in opened)
+
+            for neigh_elephant in valves[elephant]:
+                newstate = (time+1, me, neigh_elephant, score+flow, tuple(opened))
+                stack.append(newstate)
+
+            if elephantcanopen: # (4) Both open valve
+                opened.add(elephant)
+                flow = sum(flows[loc] for loc in opened)
+
+                newstate = (time+1, me, elephant, score+flow, tuple(opened))
+                stack.append(newstate)
+
+    return best
+ 
+valves, flows = getvalves(getinput('16'))
+
+print('Part 1:', dfs(flows, valves))
+print('Part 2:', elephantdfs(flows, valves))
