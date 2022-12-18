6c6a732f3edc1dbbcc9dd776e5c9e286c0bd91a4
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-18 14:01:42 +0100

    Day 17 part 2 with 1 cycle detection

--- solutions/day-17.py
+++ solutions/day-17.py
@@ -2,27 +2,15 @@
 
 from _getinput import *
 
-moves = list(map(str,getinput('17')))
-
-shapes = {'-': [(2,0), (3,0), (4,0), (5,0)], 
-          '+': [(2,1), (3,0), (3,1), (3,2), (4,1)], 
-          'L': [(2,0), (3,0), (4,0), (4,1), (4,2)], 
-          '|': [(2,0), (2,1), (2,2), (2,3)],
-          'o': [(2,0), (3,0), (2,1), (3,1)]}
-
-order = ['-','+','L','|','o']
-
-height = 0 # How high is the tower
-rockpos = set() # Keep track of all the rocks positions
-
 def tetris(shape, moves, rockpos, height):
     
     rest = False
     
     while not rest:
 
-        move = moves.pop(0); moves.append(move)
-        
+        imove = imoves.pop(0); imoves.append(imove)
+        move = moves[imove]
+
         # Move left/right
 
         moved = [c.copy() for c in shape]
@@ -65,14 +53,63 @@ def tetris(shape, moves, rockpos, height):
         else: # can drop
             for c in shape: c[1] -= 1 # shape drops one down
 
-    return rockpos, height, moves
+    return rockpos, height, moves, imove, shape
 
-for i in range(2022):
-    shape = order.pop(0); order.append(shape)
+
+moves = list(map(str,getinput('17')))
+
+shapes = {'-': [(2,0), (3,0), (4,0), (5,0)], 
+          '+': [(2,1), (3,0), (3,1), (3,2), (4,1)], 
+          'L': [(2,0), (3,0), (4,0), (4,1), (4,2)], 
+          '|': [(2,0), (2,1), (2,2), (2,3)],
+          'o': [(2,0), (3,0), (2,1), (3,1)]}
+
+order = ['-','+','L','|','o']
+
+ishapes = list(range(len(order)))
+imoves = list(range(len(moves)))
+
+height = 0 # How high is the tower
+rockpos = set() # Keep track of all the rocks in tower
+
+states = {}; keys = []
+
+
+for i in range(int(1e12)):
+
+    state = (ishapes[0], imoves[0])
+
+    if state in states:
+        print("Cycle!")
+        ibeg, hbeg = states[state]
+        icycle, hcycle = i-ibeg, height-hbeg
+
+        div, mod = divmod(1e12-ibeg, icycle)
+        div, mod = int(div), int(mod)
+        print(icycle, hcycle, ibeg, hbeg)
+
+        if mod > 0:
+            print("Rest cycle")
+            imod = ibeg + mod-1
+            key = keys[imod]
+            irest, hrest = states[key]
+            total = hcycle*div + hrest
+            print(total)
+        else: 
+            print("No rest cycle")
+            total = hcycle*div + hbeg
+            print(total)
+
+        break
+
+    else: 
+        states[state] = i, height
+        keys.append(state)
+
+    ishape = ishapes.pop(0); ishapes.append(ishape)
+    shape = order[ishape]
     shape = [list(c) for c in shapes[shape]]
 
     for c in shape: c[1] += height + 3
 
-    rockpos, height, moves = tetris(shape, moves, rockpos, height)
-
-print(height, len(rockpos))
+    rockpos, height, moves, imove, shape = tetris(shape, moves, rockpos, height)
