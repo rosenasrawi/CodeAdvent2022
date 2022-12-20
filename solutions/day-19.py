7c1e1395eeddc4e4517d34971c2a29eb364e4510
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-20 15:30:42 +0100

    Refactor day 19

--- solutions/day-19.py
+++ solutions/day-19.py
@@ -5,33 +5,33 @@ from _getinput import *
 from collections import deque
 import re
 
-def testblueprint(BP, limit):
+def testblueprint(blueprint, limit):
 
-    bp_num, ore4ore, ore4clay, ore4obs, clay4obs, ore4geo, obs4geo  = BP
-    max_ore = max(ore4ore, ore4clay, ore4obs, ore4geo)
+    bp, o4o, o4c, o4ob, c4ob, o4g, ob4g = blueprint
+    omax = max(o4o, o4c, o4ob, o4g)
 
     start = ((1,0,0,0), (0,0,0,0), limit)
 
     queue = deque([start])
     visited = set()
 
-    max_geo = 0
+    maxgeode = 0
 
     while queue:
 
         robots, resources, time = queue.pop()
 
-        ore_rob, clay_rob, obs_rob, geo_robs = robots
+        orob, crob, obrob, grob = robots
         ore, clay, obs, geo = resources
 
-        if geo > max_geo: max_geo = geo
+        if geo > maxgeode: maxgeode = geo
 
         if time == 0:
             continue
 
-        ore = min(ore, max_ore + (max_ore - ore_rob) * (time -1))
-        clay = min(clay, clay4obs + (clay4obs - clay_rob) * (time -1))
-        obs = min(obs, obs4geo + (obs4geo - obs_rob) * (time -1))
+        ore = min(ore, omax + (omax - orob) * (time -1))
+        clay = min(clay, c4ob + (c4ob - crob) * (time -1))
+        obs = min(obs, ob4g + (ob4g - obrob) * (time -1))
 
         state = (robots, (ore,clay,obs,geo), time)
 
@@ -40,56 +40,56 @@ def testblueprint(BP, limit):
 
         visited.add(state)
 
-        if ore >= ore4ore and ore_rob < max_ore: # Make ore robot
+        if ore >= o4o and orob < omax: # Make ore robot
             
-            robots = (ore_rob +1, clay_rob, obs_rob, geo_robs)
-            resources = (ore + ore_rob - ore4ore, clay + clay_rob, obs + obs_rob, geo + geo_robs)
+            robots = (orob +1, crob, obrob, grob)
+            resources = (ore + orob - o4o, clay + crob, obs + obrob, geo + grob)
             
             queue.append((robots, resources, time -1))
 
-        if ore >= ore4clay and clay_rob < clay4obs: # Make clay robot
+        if ore >= o4c and crob < c4ob: # Make clay robot
 
-            robots = (ore_rob, clay_rob +1, obs_rob, geo_robs)
-            resources = (ore + ore_rob - ore4clay, clay + clay_rob, obs + obs_rob, geo + geo_robs)
-
-            queue.append((robots, resources, time -1))
-
-        if ore >= ore4obs and clay >= clay4obs and obs_rob < obs4geo: # Make obs robot
-
-            robots = (ore_rob, clay_rob, obs_rob +1, geo_robs)
-            resources = (ore + ore_rob - ore4obs, clay + clay_rob - clay4obs, obs + obs_rob, geo + geo_robs)
+            robots = (orob, crob +1, obrob, grob)
+            resources = (ore + orob - o4c, clay + crob, obs + obrob, geo + grob)
 
             queue.append((robots, resources, time -1))
 
-        if ore >= ore4geo and obs >= obs4geo: # Make geo robot
+        if ore >= o4ob and clay >= c4ob and obrob < ob4g: # Make obs robot
 
-            robots = (ore_rob, clay_rob, obs_rob, geo_robs +1)
-            resources = (ore + ore_rob - ore4geo, clay + clay_rob, obs + obs_rob - obs4geo, geo + geo_robs)
+            robots = (orob, crob, obrob +1, grob)
+            resources = (ore + orob - o4ob, clay + crob - c4ob, obs + obrob, geo + grob)
+
+            queue.append((robots, resources, time -1))
+
+        if ore >= o4g and obs >= ob4g: # Make geo robot
+
+            robots = (orob, crob, obrob, grob +1)
+            resources = (ore + orob - o4g, clay + crob, obs + obrob - ob4g, geo + grob)
 
             queue.append((robots, resources, time -1))
 
         # Or, don't make a robot
-        robots = (ore_rob, clay_rob, obs_rob, geo_robs)
-        resources = (ore + ore_rob, clay + clay_rob, obs + obs_rob, geo + geo_robs)
+        robots = (orob, crob, obrob, grob)
+        resources = (ore + orob, clay + crob, obs + obrob, geo + grob)
 
         queue.append((robots, resources, time -1))
 
-    return bp_num, max_geo
+    return bp, maxgeode
 
 blueprints = getinput('19')
 blueprints = [list(map(int, re.findall(r'\d+', bp))) for bp in blueprints]
 
 all = 0; three = 1
 
-for BP in blueprints:
-    bp_num, max_geo = testblueprint(BP, limit = 24)
+for blueprint in blueprints:
+    bp, maxgeode = testblueprint(blueprint, limit = 24)
 
-    all += (bp_num * max_geo)
+    all += (bp * maxgeode)
 
-for BP in blueprints[:3]:
-    bp_num, max_geo = testblueprint(BP, limit = 32)
+for blueprint in blueprints[:3]:
+    bp, maxgeode = testblueprint(blueprint, limit = 32)
 
-    three *= max_geo
+    three *= maxgeode
 
 print('Part 1:', all)
 print('Part 2:', three)
