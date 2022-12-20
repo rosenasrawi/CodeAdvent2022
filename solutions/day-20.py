cb6dbe6381ea086a497a3bc0faa76cc630df96b3
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-12-20 15:20:09 +0100

    Refactor day 20

--- solutions/day-20.py
+++ solutions/day-20.py
@@ -7,54 +7,49 @@ def mixing(mixed, index, imax, imin):
     for i in range(len(mixed)):
         imove = index.index(i)
         move = mixed[imove]
+        inext = imove+move
 
         if move != 0:
-            inext = imove+move
 
-            if inext > imax:
+            if inext > imax: 
                 inext %= imax
-
-            if inext < imin:
+            if inext < imin: 
                 inext %= -imax
+            if inext == 0: 
+                inext = imax
 
             n = mixed.pop(imove)
             id = index.pop(imove)
 
-            if inext == 0:
-                mixed.append(n)
-                index.append(id)
-            else:
-                mixed.insert(inext, n)
-                index.insert(inext, id)
+            mixed.insert(inext, n)
+            index.insert(inext, id)
 
     return mixed, index, imax
 
 def getgrove(mixed, imax):
 
-    izero = mixed.index(0)
-    coor = [(izero + n) % (imax + 1) for n in [1000,2000,3000]]
+    i0 = mixed.index(0)
+    coor = [(i0 + n) % (imax + 1) for n in [1000,2000,3000]]
 
     grove = sum(mixed[c] for c in coor)
 
     return grove
 
-def positioning(numbers, decrypt = False, key = 811589153):
+def positioning(numbers, nmix = 1, decrypt = False, key = 811589153):
 
     mixed = numbers.copy()
     index = list(range(len(mixed)))
     imax = index[-1]; imin = -(imax+1)
 
-    if decrypt: 
+    if decrypt:
         mixed = [num * key for num in mixed]
-        
-        for i in range(9):
-            mixed, index, imax = mixing(mixed, index, imax, imin)
 
-    mixed, index, imax = mixing(mixed, index, imax, imin)
+    for i in range(nmix):
+        mixed, index, imax = mixing(mixed, index, imax, imin)
 
     return getgrove(mixed, imax)
 
 numbers = list(map(int, getinput('20')))
 
 print('Part 1:', positioning(numbers))
-print('Part 2:', positioning(numbers, decrypt = True))
+print('Part 2:', positioning(numbers, nmix = 10, decrypt = True))
